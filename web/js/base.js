9d0fcc02965387053826efa0b3fb22ecd231c81a
kakajent <576012333@qq.com>
2016-05-04 20:46:03 +0800

    tets

---
+++ web/js/base.js
@@ -0,0 +1,8 @@
+function showDiv(){ 
+document.getElementById('mask').style.display='block';
+document.getElementById('popDiv').style.display='block'; 
+} 
+function closeDiv(){ 
+document.getElementById("mask").style.display="none";
+document.getElementById('popDiv').style.display='none'; 
+} 
