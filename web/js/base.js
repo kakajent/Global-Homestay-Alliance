869112c09e1794854ea6295361235c81c9f92ee8
kakajent <576012333@qq.com>
2016-06-29 00:33:46 +0800

    fixed

--- web/js/base.js
+++ web/js/base.js
@@ -95,5 +95,17 @@ $(document).ready(function () {
 		$('.btn-add-account').show();
 		$('.new-account').hide();
 	});
+
+	//
+	$('.form_datetime').datetimepicker({
+		//language:  'fr',
+		weekStart: 1,
+		todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		forceParse: 0,
+		showMeridian: 1
+	});
 });
 
